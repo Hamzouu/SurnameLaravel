27fbc13f67f2cdcf1704e6565bde05d1d07af1c4
Hamza Elaamely <hamelaamely@etmlnet.local>
2022-04-01 10:17:01 +0200

    Tuto Laraguide

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    return view('Welcome');
+
+});
+
+
+
+Route::get('/bonjour/{prenom}', function () {
+    $prenom = request('prenom');
+    return view('bonjour', [
+    
+        'prenom' => $prenom,
+    ]);
+
+});
+
+Route::get('/inscription', function () {
+    
+    return view('inscription');
+
+});
